14c5438765c31eb10ab74514d30ae1c69e5f1f84
Aarav Uniyal <68441178+Aarav-Uniyal@users.noreply.github.com>
2024-08-13 11:21:57 +0530

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-x = pygame.init()  # This line is important as it initialises all the pygame modules.
-print(x)  # (x, y) :- x are the successfully initialised modules and y is the opposite of x.
-game_window = pygame.display.set_mode((1200, 500))  # This line is important as it creates the game window.
-# 1200 and 500 are the width and length of the window respectively.
